6b837423f6b71b0e06fab5f4b3aa1dc133ccdd7c
“damir_gritsinenko” <gritsinenko.damir@bk.ru>
2023-10-09 13:39:10 +0300

    test

--- qsoft/assets/js/modules/input-mask.js
+++
@@ -1,9 +0,0 @@
-import inputmask from "inputmask";
-
-const ELEMENTS_SELECTOR = {
-  inputs: "[data-inputmask]",
-};
-
-export default function () {
-  Inputmask().mask($(ELEMENTS_SELECTOR.inputs));
-}
